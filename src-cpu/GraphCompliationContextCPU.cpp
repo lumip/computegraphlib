39bd700feb70f89fa79362f2a382e5c0326066d1
Lukas Prediger <lukas.prediger@rtwh-aachen.de>
2017-11-07 21:41:17 +0900

    GraphCompilationContext CPU implementations of AllocateMemory and DeallocateAllMemory are now using static_casts instead of reinterpret_casts.

--- src-cpu/GraphCompliationContextCPU.cpp
+++ src-cpu/GraphCompliationContextCPU.cpp
@@ -3,14 +3,14 @@
 
 GraphCompilationContext::NodeMemoryHandle GraphCompilationContext::AllocateMemory(size_t size)
 {
-    return reinterpret_cast<NodeMemoryHandle>(new float[size]);
+    return static_cast<NodeMemoryHandle>(new float[size]);
 }
 
 void GraphCompilationContext::DeallocateAllMemory()
 {
     for (auto elem : _memoryDescriptors)
     {
-        NodeMemoryHandle buf = reinterpret_cast<NodeMemoryHandle>(elem.first);
+        NodeMemoryHandle buf = static_cast<NodeMemoryHandle>(elem.first);
         delete[](buf);
     }
 }
