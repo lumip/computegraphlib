14e8d740a9bb0a43fba17ecc22d1a6daa76a45da
Lukas Prediger <lukas.prediger@rtwh-aachen.de>
2017-12-01 00:22:50 +0900

    Added simple graph integration test that computes softmax(X*Y+b) for matrices X*Y and vector b (broadcasted) to verify correctness of graph computation beyond single node behavior.

---
+++ tests/1gc02-Graph.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+
+#include "types.hpp"
+#include "nodes/nodes.hpp"
+#include "CompilationMemoryMap.hpp"
+#include "GraphCompiler.hpp"
+#include "CompiledGraph.hpp"
+#include "ImplementationStrategyFactory.hpp"
+
+int main(int argc, const char * const argv[])
+{
+    const size_t InputDim = 4;
+    const size_t BatchSize = 5;
+    const size_t OutputDim = 2;
+
+
+    // very simple graph which computes: softmax(X*Y+b)
+    // declare inputs and variables
+    InputNode X("X", InputDim); // BatchSize x InputDim
+    VariableNode Y("Y", InputDim, OutputDim); // InputDim x OutputDim
+    VariableNode b("b", 1, OutputDim); // 1 x OutputDim
+
+    // weighting inputs and applying bias
+    MatrixMultNode XY(&X, &Y); // BatchSize x OutputDim
+    VectorAddNode XYb(&XY, &b);
+
+    // apply softmax
+    ExpFuncNode expXYB(&XYb); // BatchSize x OutputDim
+    ReduceSumNode sumexpXYb(&expXYB, 1); // BatchSize x 1
+    VectorDivNode softmax(&expXYB, &sumexpXYb); // BatchSize x OutputDim
+
+    // specify concrete input dimensions for compilation
+    InputDimensionsMap inputDimensions;
+    inputDimensions.emplace("X", MemoryDimensions({BatchSize, InputDim}));
+    inputDimensions.emplace("Y", MemoryDimensions({InputDim, OutputDim}));
+    inputDimensions.emplace("b", MemoryDimensions({1, OutputDim}));
+
+    // compile the graph
+    GraphCompiler compiler(std::unique_ptr<const ImplementationStrategyFactory>(new ImplementationStrategyFactory));
+    const std::unique_ptr<CompiledGraph> graph = compiler.Compile(&softmax, inputDimensions);
+
+    // prepare inputs
+    InputDataMap inputDataMap;
+    DataBuffer dataX = { 1,2,3,4,
+                         2,3,4,5,
+                         3,4,5,6,
+                         4,5,6,7,
+                         5,6,7,8 };
+
+    DataBuffer dataY = { 0.5f, 0.5f,
+                         0.75f, 0.25f,
+                         0.25f, 0.75f,
+                         0.9f, 0.1f };
+
+    DataBuffer datab = { 0.33f, -0.33f };
+
+    DataBuffer expected = { 0.96643072f, 0.03356923f,
+                            0.98463225f, 0.01536771f,
+                            0.99303585f, 0.00696409f,
+                            0.99685884f, 0.00314121f,
+                            0.99858618f, 0.00141388f };
+
+    inputDataMap.emplace("X", dataX);
+    inputDataMap.emplace("Y", dataY);
+    inputDataMap.emplace("b", datab);
+
+    // evaluate graph
+    graph->Evaluate(inputDataMap);
+
+    // otbain result
+    DataBuffer result(OutputDim);
+    graph->GetNodeData(&softmax, result);
+
+    // compute and return squared error
+    float error = 0.0f;
+    for (size_t i = 0; i < OutputDim; ++i)
+    {
+        error += std::pow(result[i] - expected[i], 2);
+    }
+    std::cout << "Error: " << error << std::endl;
+
+    // return 0 if error below threshold, -1 otherwise
+    if (error < 0.00001)
+    {
+        return 0;
+    }
+    return -1;
+}
