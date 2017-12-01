35b4616f00f956403c2c820cfc5543ce3cce93be
Lukas Prediger <lukas.prediger@rtwh-aachen.de>
2017-12-02 01:14:41 +0900

    Small fix in error outputs on missing command line parameter for MNIST graph tests.

--- tests/1gp02b-MNISTGraphForwardOnlyBaseline.cpp
+++ tests/1gp02b-MNISTGraphForwardOnlyBaseline.cpp
@@ -103,7 +103,7 @@ int main(int argc, const char * const argv[])
     // Check command line arguments
     if (argc < 2)
     {
-        std::cout << "Usage: " << argv[0] << " <path to MNIST dataset>";
+        std::cout << "Usage: " << argv[0] << " <path to MNIST dataset>" << std::endl;
         return -1;
     }
     const std::string mnistDataDir(argv[1]);
--- tests/1gp03b-MNISTGraphBaseline.cpp
+++ tests/1gp03b-MNISTGraphBaseline.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main()
+int main(int argc, const char * const argv[])
 {
-    std::cout << "todo: implement" << std::endl;
+    // Check command line arguments
+    if (argc < 2)
+    {
+        std::cout << "Usage: " << argv[0] << " <path to MNIST dataset>" << std::endl;
+        return -1;
+    }
+    const std::string mnistDataDir(argv[1]);
+
+    return 0;
 }
