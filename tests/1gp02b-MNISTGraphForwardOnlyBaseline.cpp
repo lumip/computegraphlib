655a09e6fbaac8274144bce8f9a116ef9feeee31
Lukas Prediger <lukas.prediger@rtwh-aachen.de>
2017-12-03 01:14:55 +0900

    1gp02b-MNISTGraphForwardOnlyBaseline.cpp: Fixed buffer sizes to be multiple of 8 in all dimensions, simplified existing AVX code and added AVX instructions in remaining computation functions.

--- tests/1gp02b-MNISTGraphForwardOnlyBaseline.cpp
+++ tests/1gp02b-MNISTGraphForwardOnlyBaseline.cpp
@@ -2,18 +2,26 @@
 #include <algorithm>
 #include <random>
 #include <cmath>
-//#include <avxintrin.h>
+#include "immintrin.h"
 #include <assert.h>
 
 #include <mnist/mnist_reader.hpp>
 #include <papi.h>
 
 // global definitions for data dimensions
-const size_t InputDim = 784; // multiple of 8
-const size_t BatchSize = 500; //504 or 60000 would be multiples of 8
+const size_t InputDim = 784;
+const size_t InputDimMem = 784; // multiple of 8
+const size_t BatchSize = 500;
+const size_t BatchSizeMem = 504; // multiple of 8
 const size_t OutputDim = 10;
+const size_t OutputDimMem = 16; // multiple of 8
 
 typedef float* DataBuffer;
+
+inline size_t getIndex(size_t i, size_t j, size_t stride)
+{
+    return i * stride + j;
+}
 
 void initializeData(const std::string& mnistDataDir, DataBuffer& inputs, DataBuffer& classes, DataBuffer& weights, DataBuffer& bias)
 {
@@ -22,140 +30,128 @@ void initializeData(const std::string& mnistDataDir, DataBuffer& inputs, DataBuf
             mnist::read_dataset_direct<std::vector, std::vector<float>, uint8_t>(mnistDataDir, 0, 0);
 
     // copy first batch into input buffer and convert labels into one-hot vector representation
-    std::fill_n(classes, BatchSize * OutputDim, 0.0f);
-    //DataBuffer::iterator it = std::begin(inputs);
-    DataBuffer it = inputs;
+    std::fill_n(classes, BatchSizeMem * OutputDimMem, 0.0f);
     for (size_t i = 0; i < BatchSize; ++i)
     {
         const std::vector<float>& sampleImageBuffer = dataset.training_images[i];
-        it = std::copy(std::begin(sampleImageBuffer), std::end(sampleImageBuffer), it);
+        std::copy(std::begin(sampleImageBuffer), std::end(sampleImageBuffer), inputs + i * InputDimMem);
         uint8_t sampleLabel = dataset.training_labels[i];
-        classes[i * OutputDim + sampleLabel] = 1.0f;
+        classes[i * OutputDimMem + sampleLabel] = 1.0f;
     }
-    for (size_t i = 0; i < BatchSize * InputDim; ++i)
+    for (size_t i = 0; i < BatchSizeMem * InputDimMem; ++i)
     {
         inputs[i] /= 255.0f;
-        //inputs[i] = i;
     }
 
     // fill weights and bias with random numbers
     std::mt19937 gen(22); // with fixed seed, reproducible results
     std::uniform_real_distribution<float> dist(-1.f, 1.f);
 
-    std::generate_n(weights, InputDim * OutputDim, [&dist, &gen]()->float {return dist(gen);});
-    /*for (size_t i = 0; i < InputDim * OutputDim; ++i)
+    //std::generate_n(weights, InputDim * OutputDim, [&dist, &gen]()->float {return dist(gen);});
+    for (size_t i = 0; i < InputDimMem; ++i)
     {
-        weights[i] = i;
-    }*/
+        for (size_t j = 0; j < OutputDim; ++j)
+        {
+            size_t id = getIndex(i, j, OutputDimMem);
+            weights[id] = dist(gen);
+        }
+    }
     std::generate_n(bias, OutputDim, [&dist, &gen]()->float {return dist(gen);}); // we write the bias values into an additional row of inputs
 }
 
-inline size_t getIndex(size_t i, size_t j, size_t stride)
+void multiplyInputAndWeights(const DataBuffer matA, DataBuffer matB, DataBuffer matRes)
 {
-    return i * stride + j;
-}
-
-void multiplyInputAndWeights(const DataBuffer matA, DataBuffer matB, DataBuffer matRes, size_t m, size_t n, size_t d)
-{
-    assert(d*n % 8 == 0);
-    std::fill_n(matRes, m * n, 0.0f);
-    for (size_t i = 0; i < m; ++i)
+    assert(OutputDimMem % 8 == 0); // :(
+    std::fill_n(matRes, BatchSizeMem * OutputDimMem, 0.0f);
+    for (size_t i = 0; i < BatchSize; ++i)
     {
-        assert(n == 10); // sorry
-        __m256 buf[5];
-        for (size_t r = 0; r < 5; ++r)
+        for (size_t k = 0; k < InputDim; ++k)
         {
-            buf[r] = _mm256_setzero_ps();
-        }
-        size_t bufId = 0;
-        for (size_t kj = 0; kj < d * n; kj+=8)
-        {
-            //size_t j = kj % n;
-            size_t ks[8] = { (kj+0)/n, (kj+1)/n, (kj+2)/n, (kj+3)/n,
-                             (kj+4)/n, (kj+5)/n, (kj+6)/n, (kj+7)/n };
-            float aBuf[8]__attribute__ ((aligned (32))) = { matA[getIndex(i, ks[0], d)], matA[getIndex(i, ks[1], d)],
-                           matA[getIndex(i, ks[2], d)], matA[getIndex(i, ks[3], d)],
-                           matA[getIndex(i, ks[4], d)], matA[getIndex(i, ks[5], d)],
-                           matA[getIndex(i, ks[6], d)], matA[getIndex(i, ks[7], d)] };
-            __m256 aVec = _mm256_load_ps(aBuf);
-            __m256 bVec = _mm256_load_ps(&matB[kj]);
-            buf[bufId] = _mm256_add_ps(_mm256_mul_ps(aVec, bVec), buf[bufId]);
-            bufId = (bufId + 1) % 5;
-        }
-        size_t rowStart = getIndex(i, 0, n);
-        _mm256_storeu_ps(&matRes[rowStart], buf[0]);
-        __m256i mask = _mm256_setr_epi32(-1, -1, 0, 0, 0, 0, 0, 0);
-        _mm256_maskstore_ps(&matRes[rowStart+8], mask, buf[1]);
-
-        __m256 tmp;
-        mask = _mm256_setr_epi32(0, 0, -1, -1, -1, -1, -1, -1);
-        tmp  = _mm256_add_ps(_mm256_maskload_ps(&matRes[rowStart-2], mask), buf[1]);
-        _mm256_maskstore_ps(&matRes[rowStart-2], mask, tmp);
-
-        mask = _mm256_setr_epi32(-1, -1, -1, -1, 0, 0, 0, 0);
-        tmp  = _mm256_add_ps(_mm256_maskload_ps(&matRes[rowStart+6], mask), buf[2]);
-        _mm256_maskstore_ps(&matRes[rowStart+6], mask, tmp);
-        mask = _mm256_setr_epi32(0, 0, 0, 0, -1, -1, -1, -1);
-        tmp  = _mm256_add_ps(_mm256_maskload_ps(&matRes[rowStart-4], mask), buf[2]);
-        _mm256_maskstore_ps(&matRes[rowStart-4], mask, tmp);
-
-        mask = _mm256_setr_epi32(-1, -1, -1, -1, -1, -1, 0, 0);
-        tmp  = _mm256_add_ps(_mm256_maskload_ps(&matRes[rowStart+4], mask), buf[3]);
-        _mm256_maskstore_ps(&matRes[rowStart+4], mask, tmp);
-        mask = _mm256_setr_epi32(0, 0, 0, 0, 0, 0, -1, -1);
-        tmp  = _mm256_add_ps(_mm256_maskload_ps(&matRes[rowStart-6], mask), buf[3]);
-        _mm256_maskstore_ps(&matRes[rowStart-6], mask, tmp);
-
-        tmp  = _mm256_add_ps(_mm256_loadu_ps(&matRes[rowStart+2]), buf[4]);
-        _mm256_storeu_ps(&matRes[rowStart+2], tmp);
-
-        /*for (size_t k = 0; k < d; ++k)
-        {
-            size_t aIds[8] = { i * n + k }
-            __m256 aVec = _mm256_set_ps(matA[getIndex(i, k, d), matA[getINdex]])
-            float a_ik = matA[getIndex(i, k, d)]; // linear access to A
-            for (size_t j = 0; j < n; ++j)
+            __m256 aVec = _mm256_set1_ps(matA[getIndex(i, k, InputDimMem)]);
+            for (size_t j = 0; j < OutputDim; j+=8)
             {
-                matRes[getIndex(i, j, n)] += a_ik * matB[getIndex(k, j, n)]; // linear access to both
+                __m256 bVec = _mm256_load_ps(&matB[getIndex(k, j, OutputDimMem)]);
+                __m256 rVec = _mm256_load_ps(&matRes[getIndex(i, j, OutputDimMem)]);
+                rVec = _mm256_add_ps(_mm256_mul_ps(aVec, bVec), rVec);
+                _mm256_store_ps(&matRes[getIndex(i, j, OutputDimMem)], rVec);
             }
-        }*/
-    }
-}
-
-void addBiasAndComputeSoftmax(const DataBuffer& matIn, const DataBuffer& vec, DataBuffer& matRes, size_t m, size_t n)
-{
-    for (size_t i = 0; i < m; ++i)
-    {
-        float sum = 0.0f;
-        for (size_t j = 0; j < n; ++j)
-        {
-            size_t id = getIndex(i, j, n);
-            float expVal = std::exp(matIn[id] + vec[j]);
-            sum += expVal;
-            matRes[id] = expVal;
-        }
-        float invSum = 1.0f/sum;
-        for (size_t j = 0; j < n; ++j)
-        {
-            matRes[getIndex(i, j, n)] *= invSum;
         }
     }
 }
 
-float computeLoss(const DataBuffer& softmax, const DataBuffer& classes, size_t m, size_t n)
+inline __m256 sumHorizontally(__m256 xVec)
 {
-    float totalSum = 0.0f;
-    for (size_t i = 0; i < m; ++i)
+    __m256 zeroVec = _mm256_setzero_ps();
+    xVec = _mm256_hadd_ps(_mm256_hadd_ps(xVec, zeroVec), zeroVec); // sum adjacent entries in each 128b half
+    xVec = _mm256_add_ps(_mm256_permute2f128_ps(xVec, xVec, 1), xVec); // swap 128b halves and add that, giving complete sum in pos 0 and 4
+    xVec = _mm256_permute_ps(xVec, 0b00000000); // distribute sums over entire vector
+    return xVec;
+}
+
+void addBiasAndComputeSoftmax(const DataBuffer& matIn, const DataBuffer& vec, DataBuffer& matRes)
+{
+    assert(OutputDimMem % 8 == 0);
+    for (size_t i = 0; i < BatchSize; ++i)
     {
-        float sum = 0.0f;
-        for (size_t j = 0; j < n; ++j)
+        for (size_t j = 0; j < OutputDim; j+=8)
         {
-            size_t id = getIndex(i, j, n);
-            sum += classes[id] *  std::log(softmax[id]);
+            size_t id = getIndex(i, j, OutputDimMem);
+            __m256 mVec = _mm256_load_ps(&matIn[id]);
+            __m256 vVec = _mm256_load_ps(&vec[j]);
+            __m256 rVec = _mm256_add_ps(mVec, vVec);
+            _mm256_store_ps(&matRes[id], rVec);
         }
-        totalSum += (-sum)/static_cast<float>(m);
+        for (size_t j = 0; j < OutputDim; ++j) // can't SIMDize this?
+        {
+            size_t id = getIndex(i, j, OutputDimMem);
+            matRes[id] = std::exp(matRes[id]);
+        }
+        __m256 sumVec = _mm256_setzero_ps();
+        for (size_t j = 0; j < OutputDim; j+=8)
+        {
+            size_t id = getIndex(i, j, OutputDimMem);
+            __m256 aVec = _mm256_load_ps(&matRes[id]);
+            sumVec = _mm256_add_ps(aVec, sumVec);
+        }
+        sumVec = sumHorizontally(sumVec);
+        __m256 invSumVec = _mm256_rcp_ps(sumVec);
+        for (size_t j = 0; j < OutputDim; j+=8)
+        {
+            size_t id = getIndex(i, j, OutputDimMem);
+            __m256 rVec = _mm256_load_ps(&matRes[id]);
+            rVec = _mm256_mul_ps(invSumVec, rVec);
+            _mm256_store_ps(&matRes[id], rVec);
+        }
     }
-    return totalSum;
+}
+
+float computeLoss(DataBuffer& softmax, const DataBuffer& classes)
+{
+    assert(OutputDimMem % 8 == 0);
+    __m256 invBatchSizeVec = _mm256_set1_ps(1/static_cast<float>(BatchSize));
+    __m256 totalSumVec = _mm256_setzero_ps();
+    for (size_t i = 0; i < BatchSize; ++i)
+    {
+        for (size_t j = 0; j < OutputDim; ++j)  // can't SIMDize this?
+        {
+            size_t id = getIndex(i, j, OutputDimMem);
+            softmax[id] = std::log(softmax[id]);
+        }
+
+        __m256 sumVec = _mm256_setzero_ps();
+        for (size_t j = 0; j < OutputDim; j+=8)
+        {
+            size_t id = getIndex(i, j, OutputDimMem);
+            __m256 cVec = _mm256_load_ps(&classes[id]);
+            __m256 sVec = _mm256_load_ps(&softmax[id]);
+            sumVec = _mm256_add_ps(_mm256_mul_ps(cVec, sVec), sumVec);
+        }
+        totalSumVec = _mm256_sub_ps(totalSumVec, _mm256_mul_ps(sumVec, invBatchSizeVec));
+    }
+    totalSumVec = sumHorizontally(totalSumVec);
+    float totalSum[8] __attribute__((aligned(32))) { 0.0f };
+    _mm256_store_ps(totalSum, totalSumVec);
+    return totalSum[0];
 }
 
 int main(int argc, const char * const argv[])
@@ -168,20 +164,20 @@ int main(int argc, const char * const argv[])
     }
     const std::string mnistDataDir(argv[1]);
 
-    DataBuffer inputs = static_cast<DataBuffer>(aligned_alloc(32, (BatchSize * InputDim) * sizeof(float)));
-    DataBuffer classes = static_cast<DataBuffer>(aligned_alloc(32, (BatchSize * OutputDim) * sizeof(float)));
-    DataBuffer weights = static_cast<DataBuffer>(aligned_alloc(32, (InputDim * OutputDim) * sizeof(float)));
-    DataBuffer bias = static_cast<DataBuffer>(aligned_alloc(32, (OutputDim) * sizeof(float)));
+    DataBuffer inputs = static_cast<DataBuffer>(aligned_alloc(32, (BatchSizeMem * InputDimMem) * sizeof(float)));
+    DataBuffer classes = static_cast<DataBuffer>(aligned_alloc(32, (BatchSizeMem * OutputDimMem) * sizeof(float)));
+    DataBuffer weights = static_cast<DataBuffer>(aligned_alloc(32, (InputDimMem * OutputDimMem) * sizeof(float)));
+    DataBuffer bias = static_cast<DataBuffer>(aligned_alloc(32, (OutputDimMem) * sizeof(float)));
 
     initializeData(mnistDataDir, inputs, classes, weights, bias);
 
-    DataBuffer buffer = static_cast<DataBuffer>(aligned_alloc(32, (BatchSize * OutputDim) * sizeof(float)));
+    DataBuffer buffer = static_cast<DataBuffer>(aligned_alloc(32, (BatchSizeMem * OutputDimMem) * sizeof(float)));
 
     long long time_start = PAPI_get_real_nsec();
     long long cycs_start = PAPI_get_real_cyc();
-    multiplyInputAndWeights(inputs, weights, buffer, BatchSize, OutputDim, InputDim);
-    addBiasAndComputeSoftmax(buffer, bias, buffer, BatchSize, OutputDim);
-    float loss = computeLoss(buffer, classes, BatchSize, OutputDim);
+    multiplyInputAndWeights(inputs, weights, buffer);
+    addBiasAndComputeSoftmax(buffer, bias, buffer);
+    float loss = computeLoss(buffer, classes);
     long long time_stop = PAPI_get_real_nsec();
     long long cycs_stop = PAPI_get_real_cyc();
 
