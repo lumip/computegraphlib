30702440e92db49eb330c1bb28f84510b87ae04c
Lukas Prediger <lukas.prediger@rtwh-aachen.de>
2017-12-10 00:04:25 +0900

    1gp03-MNISTGraph: Now consumes all batches within an epoch during training. Some small fixes/changes/refactors.

--- tests/1gp03-MNISTGraph.cpp
+++ tests/1gp03-MNISTGraph.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <random>
+#include <assert.h>
 
 #include <mnist/mnist_reader.hpp>
 
@@ -15,28 +16,31 @@ class MNISTGraph
 {
 private:
     std::vector<std::unique_ptr<Node>> _nodes;
-    InputNode* inputBatch;
-    InputNode* correctClasses;
-    VariableNode* weights;
-    VariableNode* bias;
+    InputNode* _inputBatch;
+    InputNode* _correctClasses;
+    VariableNode* _weights;
+    VariableNode* _bias;
+    float const LearningRate;
 public:
-    MNISTGraph()
+    MNISTGraph() : MNISTGraph(0.1f) { }
+    MNISTGraph(float const learningRate)
         : _nodes()
-        , inputBatch(new InputNode("ImgBatch")) // constructor sets up input and variable nodes
-        , correctClasses(new InputNode("Classes"))
-        , weights(new VariableNode("Weights"))
-        , bias(new VariableNode("Bias"))
+        , _inputBatch(new InputNode("ImgBatch")) // constructor sets up input and variable nodes
+        , _correctClasses(new InputNode("Classes"))
+        , _weights(new VariableNode("Weights"))
+        , _bias(new VariableNode("Bias"))
+        , LearningRate(learningRate)
     {
-        _nodes.emplace_back(inputBatch), _nodes.emplace_back(correctClasses);
-        _nodes.emplace_back(weights); _nodes.emplace_back(bias);
+        _nodes.emplace_back(_inputBatch), _nodes.emplace_back(_correctClasses);
+        _nodes.emplace_back(_weights); _nodes.emplace_back(_bias);
     }
     ~MNISTGraph() { }
     NodePtr ConstructClassifierGraph()
     {
         // build feedforward network for classification
         // weighting inputs and applying bias
-        MatrixMultNode* weightedInputs = new MatrixMultNode(inputBatch, weights); // BatchSize x OutputDim
-        VectorAddNode* weightedBiasedInputs = new VectorAddNode(weightedInputs, bias);
+        MatrixMultNode* weightedInputs = new MatrixMultNode(_inputBatch, _weights); // BatchSize x OutputDim
+        VectorAddNode* weightedBiasedInputs = new VectorAddNode(weightedInputs, _bias);
         _nodes.emplace_back(weightedInputs); _nodes.emplace_back(weightedBiasedInputs);
 
         // apply softmax
@@ -52,7 +56,7 @@ public:
         // add loss function graph part for training
         // compute loss (cross-entropy)
         LogFuncNode* logSoftmax = new LogFuncNode(softmax); // BatchSize x OutputDim
-        VectorMultNode* vm = new VectorMultNode(correctClasses, logSoftmax); // BatchSize x OutputDim
+        VectorMultNode* vm = new VectorMultNode(_correctClasses, logSoftmax); // BatchSize x OutputDim
         ReduceSumNode* negCrossEntropy = new ReduceSumNode(vm, 1); // BatchSize x 1
         NegateNode* crossEntropy = new NegateNode(negCrossEntropy); // cross entropy per input
         ReduceMeanNode* loss = new ReduceMeanNode(crossEntropy, 0); // 1x1, mean cross entropy over batch
@@ -64,7 +68,7 @@ public:
     void ConstructBackpropagationGraph(NodePtr softmax, NodePtr loss, size_t outputDim)
     {
         // build backpropagation/derivation graph (todo: it would be nice if we could automate this later on)
-        NegateNode* negCorrectClasses = new NegateNode(correctClasses); // BatchSize x OutputDim
+        NegateNode* negCorrectClasses = new NegateNode(_correctClasses); // BatchSize x OutputDim
         VectorAddNode* gradFactors = new VectorAddNode(softmax, negCorrectClasses); // BatchSize x OutputDim
         ReduceMeanNode* biasGrad = new ReduceMeanNode(gradFactors, 0); // 1 x OutputDim
         _nodes.emplace_back(negCorrectClasses); _nodes.emplace_back(gradFactors); _nodes.emplace_back(biasGrad);
@@ -72,7 +76,7 @@ public:
         for (size_t i = 0; i < outputDim; ++i)
         {
             SliceNode* slice = new SliceNode(gradFactors, i, 1); // slice i from dim 1, BatchSize x 1
-            VectorMultNode* tmp = new VectorMultNode(slice, inputBatch); // BatchSize x InputDim
+            VectorMultNode* tmp = new VectorMultNode(slice, _inputBatch); // BatchSize x InputDim
             ReduceMeanNode* reduced = new ReduceMeanNode(tmp, 0); // 1 x InputDim
             _nodes.emplace_back(slice); _nodes.emplace_back(tmp); _nodes.emplace_back(reduced);
             weightGradComponents[i] = reduced;
@@ -81,21 +85,20 @@ public:
         TransposeNode* weightGrad = new TransposeNode(weightGradTransp); // InputDim x OutputDim
         _nodes.emplace_back(weightGradTransp); _nodes.emplace_back(weightGrad);
 
-        const float LearningRate = 0.01f;
         ConstMultNode* biasUpdateDelta = new ConstMultNode(biasGrad, LearningRate); // 1 x OutputDim
         NegateNode* biasUpdateDeltaNeg = new NegateNode(biasUpdateDelta); // 1 x OutputDim
-        VectorAddNode* biasUpdated = new VectorAddNode(bias, biasUpdateDeltaNeg); // 1 x OutputDim
-        bias->SetInput(biasUpdated);
+        VectorAddNode* biasUpdated = new VectorAddNode(_bias, biasUpdateDeltaNeg); // 1 x OutputDim
+        _bias->SetInput(biasUpdated);
         _nodes.emplace_back(biasUpdateDelta); _nodes.emplace_back(biasUpdateDeltaNeg); _nodes.emplace_back(biasUpdated);
 
         ConstMultNode* weightsUpdateDelta = new ConstMultNode(weightGrad, LearningRate); // InputDim x OutputDim
         NegateNode* weightsUpdateDeltaNeg = new NegateNode(weightsUpdateDelta); // InputDim x OutputDim
-        VectorAddNode* weightsUpdated = new VectorAddNode(weights, weightsUpdateDeltaNeg); // InputDim x OutputDim
-        weights->SetInput(weightsUpdated);
+        VectorAddNode* weightsUpdated = new VectorAddNode(_weights, weightsUpdateDeltaNeg); // InputDim x OutputDim
+        _weights->SetInput(weightsUpdated);
         _nodes.emplace_back(weightsUpdateDelta); _nodes.emplace_back(weightsUpdateDeltaNeg); _nodes.emplace_back(weightsUpdated);
     }
-    ConstNodePtr GetWeightsNode() const { return weights; }
-    ConstNodePtr GetBiasNode() const { return bias; }
+    ConstNodePtr GetWeightsNode() const { return _weights; }
+    ConstNodePtr GetBiasNode() const { return _bias; }
 };
 
 class MNISTDataset
@@ -111,7 +114,7 @@ public:
         , OutputDim(outputDim)
         , _trainingCursor(0)
         , _testCursor(0)
-    {}
+    { }
     size_t GetTrainingSampleCount() const { return _dataset.training_images.size(); }
     size_t GetTestSampleCount() const { return _dataset.test_images.size(); }
     bool GetTrainingBatch(DataBuffer& inputs, DataBuffer& classes, const size_t batchSize)
@@ -168,6 +171,10 @@ int main(int argc, const char * const argv[])
     const size_t BatchSize = 500;
     const size_t OutputDim = 10;
 
+    // ####### load training data #######
+    MNISTDataset dataset(mnistDataDir, OutputDim);
+    assert(dataset.GetTrainingSampleCount() % BatchSize == 0); // to simplify stuff: assert that BatchSize divides available training samples evenly
+
     // ####### use helper class for graph setup #######
     MNISTGraph graphTemplate;
     NodePtr softmax = graphTemplate.ConstructClassifierGraph();
@@ -199,11 +206,10 @@ int main(int argc, const char * const argv[])
     variablesDataMap.emplace("Bias", biasData);
     graph->InitializeVariables(variablesDataMap);
 
-    // ####### read input data #######
-    MNISTDataset dataset(mnistDataDir, OutputDim);
+    // ####### fetch input data #######
     DataBuffer imgInputData(BatchSize * InputDim);
     DataBuffer classesInputData(BatchSize * OutputDim);
-    dataset.GetTrainingBatch(imgInputData, classesInputData, BatchSize);
+    //dataset.GetTrainingBatch(imgInputData, classesInputData, BatchSize);
 
     // ####### prepare input feeding dict #######
     InputDataMap inputDataMap;
@@ -213,18 +219,30 @@ int main(int argc, const char * const argv[])
     // ####### run training iterations until loss converges #######
     float previousLoss = std::numeric_limits<float>::infinity();
     float currentLoss = std::numeric_limits<float>::infinity();
-    const size_t StopThreshold = 1e-7;
+    const float StopThreshold = 1e-1;
     DataBuffer lossOutput(1);
     do
     {
         previousLoss = currentLoss;
-        graph->Evaluate(inputDataMap);
+        float epochLoss = 0.0f;
+        size_t epochBatchCount = 0;
+        bool moreBatches = false;
+        do
+        {
+            moreBatches = dataset.GetTrainingBatch(imgInputData, classesInputData, BatchSize);
+            graph->Evaluate(inputDataMap);
 
-        graph->GetNodeData(loss, lossOutput);
-        currentLoss = lossOutput[0];
+            graph->GetNodeData(loss, lossOutput);
+            epochLoss += lossOutput[0];
+            ++epochBatchCount;
+        } while (!moreBatches);
+
+        currentLoss = epochLoss / static_cast<float>(epochBatchCount);
         std::cout << "Loss: " << currentLoss << std::endl;
     } while (previousLoss - currentLoss > StopThreshold);
 
+
+    // ######################### EVALUATION #########################
     // ####### set up and compile evaluation graph (forward classification graph only) #######
     const size_t TestSampleCount = dataset.GetTestSampleCount();
     MNISTGraph evaluationGraphTemplate;
@@ -253,6 +271,7 @@ int main(int argc, const char * const argv[])
     evalGraph->Evaluate(evalInputDataMap);
 
     // ####### retrieve predictions and calculate classifier precision #######
+    // note: this could also be a graph computation
     evalGraph->GetNodeData(evalSoftmax, evalPredictions);
     size_t correct = 0;
     for (size_t i = 0; i < TestSampleCount; ++i)
